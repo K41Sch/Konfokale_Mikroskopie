536b6d222086a0ccc6cb88bc6b0ec8eeaae7ed22
K41Sch <105243904+K41Sch@users.noreply.github.com>
2023-01-18 08:52:05 +0100

    added Polynomial evaluation

--- Einlesen.m
+++ Einlesen.m
@@ -1,4 +1,4 @@
-folder = 'C:/Users/oj9048/Desktop/Vormittag/cent_14_Dec_2022_11_17_51_14_Dec_2022/';
+folder = 'C:\Users\Kai\Documents\KIT Dokumente\Praktikum Mechatronische Messsysteme\V1_Konfokale_Mikroskopie\Mikroskopie\cent_14_Dec_2022_11_30_44_14_Dec_2022';
 files = dir(fullfile(folder,'*.plux.plu'));
 L = length(files);
 image_array = zeros(L,1024,1360);
@@ -25,6 +25,8 @@ end
 
 COM = sum(weighted_image,1)./sum(image_array,1);
 
+imagesc(squeeze(COM))
+
 %%
 %LSQ
 polynom = zeros(size(image_array,2),size(image_array,3),3);
@@ -33,4 +35,21 @@ for i=1:size(image_array,2)
         polynom(i,j,:) = polyfit(z,image_array(:,i,j),2);
     end
 end
+
+%% 
+poly_image = zeros(size(image_array,2),size(image_array,3),3);
+LSQ_height = zeros(size(image_array,2),size(image_array,3),1);
+LSQ_height_vector = zeros(L,1);
+
+for i=1:size(image_array,2)
+    for j=1:size(image_array,3) 
+        for k=1:L
+            LSQ_height_vector(L) = polynom(i,j,3) + polynom(i,j,2)*z(k) ... 
+                + polynom(i,j,1)*z(k).^2;
+        end
+        LSQ_height(i,j) = max(LSQ_height_vector);
+    end
+end
+
+imagesc(LSQ_height)
 
